d861e847365a6f288382088bfd8ed519e243c7a4
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-04-05 12:51:20 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+    const userInput = {
+        name: name,
+        email: email
+    };
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(userInput)
+      };
+  
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(res => res.json())
+        .then(data => {
+          document.body.innerHTML += `<p>User ID: ${data.id}</p>`;
+        })
+        .catch(error => {
+          document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+
+    }
+    submitData("Moya Wizi", "moyawizi@gmail.com");
+
+
+
+
